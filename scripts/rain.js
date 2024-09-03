f021f6a29ef68d388e1a1b995803738bc9587257
Iliya Kolahan <iliyakolahan@gmail.com>
2024-09-03 18:37:42 +0800

    rain animation update

--- scripts/rain.js
+++ scripts/rain.js
@@ -1,120 +1,109 @@
-var ctx;
-var cW;
-var cH;
-
-var raindrops;
-
-var rainStrength = 1;
-
-function initCanvas() {
-
-	ctx = document.getElementById("particleCanvas").getContext("2d");
-
-	ctx.canvas.width = 1920 * 0.75; //690
-	ctx.canvas.height = 1080 * 0.75; //540
-
-	cW = ctx.canvas.width;
-	cH = ctx.canvas.height;
-
-}
-
-function Raindrops() {
-
-	this.x;
-	this.y;
-	this.s;
-	this.width;
-	this.height;
-
-	this.drops = [];
-	this.splashes = [];
-
-}
-Raindrops.prototype.addDrop = function() {
-	
-	this.x = (Math.random() * (cW + 100)) - 100;
-	this.y = 0;
-	this.s = (Math.random() * 7) + 2;
-
-	this.drops.push({
-		x: this.x,
-		y: this.y,
-		velY: 2,
-		width: this.s / 3,
-		height: this.s * 1.2,
-		speed: this.s,
-		life: 60
-	});
-
-};
-Raindrops.prototype.render = function() {
-
-	for (var i = 0; i < rainStrength; i++) {
-		this.addDrop();
-	};
-	
-	ctx.save();
-
-	ctx.clearRect(0, 0, cW, cH);
-
-	ctx.fillStyle = 'rgb(255, 255, 255)';
-	for (var i = 0; i < this.drops.length; i++) {
-		var drop = this.drops[i];
-
-		ctx.fillRect(drop.x, drop.y, drop.width, drop.height);
-		drop.y += drop.speed * 2;
-		drop.x += 2;
-
-		if (drop.y + drop.height > cH) {
-			this.splashes.push(drop);
-
-			this.drops.splice(i, 1);
-		}
-	};
-
-	for (var i = 0; i < this.splashes.length; i++) {
-		var splash = this.splashes[i];
-
-		ctx.fillRect(splash.x, splash.y, splash.width/3, splash.height/3);
-
-		splash.y -= splash.velY * splash.speed / 6;
-		splash.life--;
-		splash.velY -= 0.1;
-		splash.x += 0.15 * splash.speed;
-
-		if (splash.life <= 0 ) {
-			this.splashes.splice(i, 1);
-		}
-		
-	};
-
-	ctx.restore();
-
-};
-
-
-function init() {
-
-	raindrops = new Raindrops();
-
-	loop();
-}
-
-function render() {
-
-	raindrops.render();
-
-}
-
-function loop() {
-
-	requestAnimationFrame(loop);
-	render();
-
-
-}
-
-window.addEventListener('load', function() {
-	initCanvas();
-	init();
-});
+let ctx, cW, cH, raindrops;
+let rainStrength = 1;
+let mouseX = 0;
+  
+  function initCanvas() {
+    const canvas = document.getElementById("particleCanvas");
+    ctx = canvas.getContext("2d");
+  
+    canvas.width = 1920 * 0.75;
+    canvas.height = 1080 * 0.75;
+  
+    cW = canvas.width;
+    cH = canvas.height;
+  
+    mouseX = cW / 2;
+  }
+  
+  class Raindrops {
+    constructor() {
+      this.drops = [];
+      this.splashes = [];
+    }
+  
+    addDrop() {
+      const x = (Math.random() * (cW + 100)) - 100;
+      const s = (Math.random() * 7) + 2;
+  
+      this.drops.push({
+        x: x,
+        y: 0,
+        velY: 2,
+        width: s / 3,
+        height: s * 1.2,
+        speed: s,
+        life: 60
+      });
+    }
+  
+    render() {
+      for (let i = 0; i < rainStrength; i++) {
+        this.addDrop();
+      }
+  
+      ctx.save();
+      ctx.clearRect(0, 0, cW, cH);
+      ctx.fillStyle = 'rgb(255, 255, 255)';
+  
+      this.drops.forEach((drop, index) => {
+        ctx.fillRect(drop.x, drop.y, drop.width, drop.height);
+  
+        const direction = (mouseX - cW / 2) * 0.01;
+        drop.x += direction;
+        drop.y += drop.speed * 2;
+  
+        if (drop.y + drop.height > cH) {
+          this.splashes.push({
+            x: drop.x + (Math.random() * 20 - 10),
+            y: drop.y,
+            width: drop.width,
+            height: drop.height,
+            velY: drop.velY,
+            speed: drop.speed,
+            life: drop.life
+          });
+  
+          this.drops.splice(index, 1);
+        }
+      });
+  
+      this.splashes.forEach((splash, index) => {
+        ctx.fillRect(splash.x, splash.y, splash.width / 3, splash.height / 3);
+  
+        splash.y -= splash.velY * splash.speed / 6;
+        splash.life--;
+        splash.velY -= 0.1;
+  
+        if (splash.life <= 0) {
+          this.splashes.splice(index, 1);
+        }
+      });
+  
+      ctx.restore();
+    }
+  }
+  
+  function init() {
+    raindrops = new Raindrops();
+    loop();
+  }
+  
+  function render() {
+    raindrops.render();
+  }
+  
+  function loop() {
+    requestAnimationFrame(loop);
+    render();
+  }
+  
+  window.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX - ctx.canvas.getBoundingClientRect().left;
+  });
+  
+  window.addEventListener('load', () => {
+    initCanvas();
+    init();
+    typewriteTitle('Arta');
+  });
+  
